7bd5d3524c230396cc4d61d7bbf8adafcab68992
Xetch <babyroselegaspi01@gmail.com>
2026-08-22 14:15:02 +0800

    Update zsub.c

--- zsub.c
+++ zsub.c
@@ -1,69 +1,54 @@
 #include "zrt.h"
 #include "syscalls.h"
 
-static int zrt_getchar(void) {
-    char c;
-    long n = sys_read(0, &c, 1);
-    if (n <= 0) return -1; // EOF
-    return (unsigned char)c;
+#define ZSUB_BUF_SIZE 65536
+
+static char g_buf[ZSUB_BUF_SIZE];
+
+static long zsub_strlen(const char *s) {
+    long n = 0;
+    while (s[n] != '\0') n++;
+    return n;
 }
 
-static void zrt_putchar(char c) {
-    sys_write(1, &c, 1);
+static void zsub_err(const char *msg) {
+    sys_write(2, msg, zsub_strlen(msg));
 }
 
-void zrt_execute(const char *code) {
-    unsigned char tape[ZRT_TAPE_SIZE] = {0};
-    unsigned char *ptr = tape;
-    int pc = 0;
-    int len = 0;
-    while (code[len] != '\0') len++;
-
-    while (code[pc] != '\0') {
-        switch (code[pc]) {
-            case '>':
-                if (ptr < tape + ZRT_TAPE_SIZE - 1) ptr++;
-                break;
-            case '<':
-                if (ptr > tape) ptr--;
-                break;
-            case '+':
-                (*ptr)++;
-                break;
-            case '-':
-                (*ptr)--;
-                break;
-            case '.':
-                zrt_putchar((char)*ptr);
-                break;
-            case ',': {
-                int c = zrt_getchar();
-                if (c != -1) *ptr = (unsigned char)c;
-                break;
-            }
-            case '[':
-                if (*ptr == 0) {
-                    int loop = 1;
-                    while (loop > 0 && pc < len - 1) {
-                        pc++;
-                        if (code[pc] == '[') loop++;
-                        else if (code[pc] == ']') loop--;
-                    }
-                }
-                break;
-            case ']':
-                if (*ptr != 0) {
-                    int loop = 1;
-                    while (loop > 0 && pc > 0) {
-                        pc--;
-                        if (code[pc] == ']') loop++;
-                        else if (code[pc] == '[') loop--;
-                    }
-                }
-                break;
-            default:
-                break;
-        }
-        pc++;
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        const char *hello_world =
+            "+++++ +++++ [ > +++++ ++ > +++++ +++++ > +++ > + <<<< - ] "
+            "> ++ . > + . +++++ ++ . . +++ . > ++ . << +++++ +++++ +++++ . "
+            "> . +++ . ----- - . ----- --- . > + . > .";
+        zrt_execute(hello_world);
+        return 0;
     }
+
+    const char *path = argv[1];
+    long len = zsub_strlen(path);
+
+    if (len < 2 || path[len - 2] != '.' || path[len - 1] != 'x') {
+        zsub_err("zsub: not a .x program\n");
+        return 1;
+    }
+
+    int fd = sys_open(path, 0 /* O_RDONLY */, 0);
+    if (fd < 0) {
+        zsub_err("zsub: could not open file\n");
+        return 1;
+    }
+
+    long total = 0;
+    long n;
+    while (total < ZSUB_BUF_SIZE - 1 &&
+           (n = sys_read(fd, g_buf + total, ZSUB_BUF_SIZE - 1 - total)) > 0) {
+        total += n;
+    }
+    g_buf[total] = '\0';
+    sys_close(fd);
+
+    zrt_execute(g_buf);
+
+    return 0;
 }
